d7c04661f0714e13e4c504be25ed1923c1bb95c9
Samuel <samuel@families.dk>
2023-10-09 14:47:37 +0200

    Viser datoen for oprettelse

--- simple-php-boilerplate-master/app/frontend/pages/comments.php
+++ simple-php-boilerplate-master/app/frontend/pages/comments.php
@@ -2,8 +2,8 @@
     <div class="row">
         <div class="jumbotron text-center" style="margin-bottom:0">
             <h1>Post title: <?php echo $post->title; ?></h1>
-            <h2>The Comment Section</h1>
-
+            <h2>The Comment Section</h2>
+            <h3> <?php echo $post->created_at; ?></h3>
                 <?php
 
 
@@ -12,6 +12,7 @@
                         echo '<div class="card">';
                         echo '<div class="card-body">';
                         echo '<h4 class="card-title">Mit id er ' . $c->user_id . '  </h4>';
+                        echo '<h5 class="card-title">Comment date: ' . $c->created_at . '</h5>';
                         echo '<p class="card-text">' . $c->content . '</p>';
 
                         if ($c->user_id === $user->data()->uid) {
@@ -32,9 +33,6 @@
 
                 ?>
                 <div class="container" style="padding-top: 5%; padding-bottom: 5%;">
-                <?php
-                            echo "Today is " . date("d/m/Y") . "<br>";
-                            ?>
                     <h2>Comment</h2>
                     <form action="" method="post">
                         <div class="form-group">
