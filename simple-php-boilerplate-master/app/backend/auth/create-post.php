9b0204cd8abec764f9be9b42787323c1ed6e9de1
Y0S3FXD <shekyousef@gmail.com>
2023-10-10 12:26:39 +0200

    Now you can upload and see image front page

--- simple-php-boilerplate-master/app/backend/auth/create-post.php
+++ simple-php-boilerplate-master/app/backend/auth/create-post.php
@@ -21,11 +21,33 @@ if (Input::exists()) {
 
         if ($validate->passed()) {
             try {
+                // Check if an image file was uploaded
+                if (!empty($_FILES['post_image']['name'])) {
+                    // Get the temporary image path
+                    $imagePath = $_FILES['post_image']['tmp_name'];
+
+                    // Upload the image to Freeimage.host
+                    $imageInfo = Post::uploadImageToHostingService($imagePath);
+
+                    if ($imageInfo !== false && isset($imageInfo['url'])) {
+                        $imageURL = $imageInfo['url'];
+                    } else {
+                        echo '<div class="alert alert-danger"><strong>Error:</strong> Image upload failed.</div>';
+                        // Handle the error as needed
+                        exit();
+                    }
+                } else {
+                    // No image was uploaded
+                    $imageURL = ''; // Set a default image URL or leave it empty
+                }
+
+                // Create the post and include the image URL
                 Post::create(array(
                     'title'  => Input::get('title'),
                     'content'  => Input::get('content'),
-                    'user_id'      => $user->data()->uid,
-                    'channel_id'    => Input::get('channel_id'),
+                    'img_url' => $imageURL, // Set the image URL here
+                    'user_id' => $user->data()->uid,
+                    'channel_id' => Input::get('channel_id'),
                 ));
 
                 Session::flash('create-post-success', 'Thanks for posting.');
@@ -34,6 +56,9 @@ if (Input::exists()) {
                 die($e->getMessage());
             }
         } else {
+            $imagePath = $_FILES['post_image']['tmp_name'];
+
+            var_dump($imagePath);
             foreach ($validate->errors() as $error) {
                 echo '<div class="alert alert-danger"><strong></strong>' . cleaner($error) . '</div>';
             }
--- simple-php-boilerplate-master/app/frontend/pages/home.php
+++ simple-php-boilerplate-master/app/frontend/pages/home.php
@@ -3,17 +3,16 @@
     <?php if ($posts->count()) {
       foreach ($posts->results() as $p) {
 
-        echo '<div class ="card">';
 
         echo '<div class ="card" style="width: 18rem;">';
-        echo '<img class="card-img-top" src="https://picsum.photos/536/354" alt="Card image cap">';
+        if(!empty($p->img_url)){
+          echo '<img class="card-img-top" src="'.$p->img_url.'" alt="Card image cap">';
+        }
         echo '<div class="card-body">';
         echo  ' <h2>' . $p->title . ' </h2>';
         echo '      <p>' . $p->content . ' </p>';
         echo ' </div>';
         echo ' </div>';
-
-        echo ' </div>';;
-      }
+      } 
     } ?>
   </div>
